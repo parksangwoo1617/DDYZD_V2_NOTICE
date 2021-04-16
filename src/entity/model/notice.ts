2adc8a48637c57c995902ba639411dc8e216c41a
parksangwoo <b01056721617@gmail.com>
2021-04-16 23:33:51 +0900

    notice Model

--- src/entity/model/notice.ts
+++ src/entity/model/notice.ts
@@ -1,4 +1,20 @@
-import { Entity, JoinColumn, ManyToOne } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne } from "typeorm";
 import { User } from "./User";
 import { Club } from "./Club";
-import { EntityWithIdColumn } from "./EntityW"
+import { EntityWithIdColumn } from "./EntityWithPrimaryColumn";
+
+@Entity('notice')
+export class Notice extends EntityWithIdColumn{
+    @Column()
+    user_id: number;
+
+    @Column()
+    title: string;
+
+    @Column({ type: "varchar", length: 3000, name: "content"})
+    content: string;
+
+    @OneToOne(type => Club, club_id => Club.id)
+    @JoinColumn()
+    club_id: number;
+}
